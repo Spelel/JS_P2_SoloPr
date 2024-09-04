6dbaf942b0814b8af549f56d9aade7823dd27c48
Arek <arek-karczo.wp.pl@wp.pl>
2024-09-04 19:50:18 +0200

    Almost done

---
+++ index.js
@@ -0,0 +1,56 @@
+let countEl = document.getElementById("count1")
+let countEl2 = document.getElementById("count2")
+
+let count1 = 0
+let count2 = 0
+
+countEl.textContent = count1
+countEl2.textContent = count2
+// --------------------------------------------------------------------
+function add1() {
+    count1 += 1
+    countEl.textContent = count1
+}
+
+function add2() {
+    count1 += 2
+    countEl.textContent = count1
+}
+
+function add3() {
+    count1 += 3
+    countEl.textContent = count1
+}
+
+// --------------------------------------------------------------------
+
+function add1_1() {
+    count2 += 1
+    countEl2.textContent = count2
+}
+
+function add2_2() {
+    count2 += 2
+    countEl2.textContent = count2
+}
+
+function add3_3() {
+    count2 += 3
+    countEl2.textContent = count2
+}
+// --------------------------------------------------------------------
+
+let saveEl = document.getElementById("save_El")
+
+
+function reset() {
+    countEl.textContent = count1
+    let s1 = "HOME: " + count1 + " / " + "GUEST: " + count2 + "   "
+
+    saveEl.textContent += s1
+    count1 = 0
+    countEl.textContent = count1
+    count2 = 0
+    countEl2.textContent = count2
+}
+
